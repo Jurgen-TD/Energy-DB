c12c77bb2ce69442dee3a16000dccc1d6e940ff6
Mühl Jürgen <muehl@Rattenkarl.fritz.box>
2025-12-09 22:10:37 +0100

    Initial setup for ETL pipeline

---
+++ main.py
@@ -0,0 +1,13 @@
+# main.py
+import datetime
+import pandas as pd
+
+
+def run_etl():
+    now = datetime.datetime.now()
+    print(f"ETL Job gestartet um: {now}")
+    print("Dies ist ein Test für GitHub Actions.")
+    # Hier kommt später der API-Code hin
+
+if __name__ == "__main__":
+    run_etl()
